7c5bba1da7976aaf050c307f09f1289b6ae92d70
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2024-07-14 02:22:11 +0800

    ndahejeje admin

--- buja_spot/admin.py
+++ buja_spot/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Item,TicketType
+from .models import Item,TicketType,EventType,EventCategory,Event,EventTicketType
 # Register your models here.
 admin.site.register(Item)
 admin.site.register(TicketType)
+admin.site.register(EventType)
+admin.site.register(EventCategory)
+admin.site.register(Event)
+admin.site.register(EventTicketType)
 
