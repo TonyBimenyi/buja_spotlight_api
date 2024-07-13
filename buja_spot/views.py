0d39b6bb79463f04f27974b3cf3e1bb44c489503
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2024-07-14 02:13:30 +0800

    view definition

--- buja_spot/views.py
+++ buja_spot/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Item,TicketType,EventType,EventCategory,Event,EventTicketType
-from .serializers import ItemSerializer,TicketTypeSerializer
+from .serializers import ItemSerializer,TicketTypeSerializer,EventTypeSerializer, EventCategorySerializer, EventCategorySerializer, EventSerializer, EventTicketTypeSerializer
 from rest_framework.response import Response
 # Create your views here.
 
@@ -19,3 +19,24 @@ class TicketTypeView(viewsets.ViewSet):
         serializer = TicketTypeSerializer(self.queryset, many = True)
         return Response(serializer.data)
 
+class TicketTypeView(viewsets.ViewSet):
+    queryset = TicketType.objects.all()
+
+    def list(self, request):
+        serializer = TicketTypeSerializer(self.queryset, many = True)
+        return Response(serializer.data)
+
+class TicketTypeView(viewsets.ViewSet):
+    queryset = TicketType.objects.all()
+
+    def list(self, request):
+        serializer = TicketTypeSerializer(self.queryset, many = True)
+        return Response(serializer.data)
+    
+
+class TicketTypeView(viewsets.ViewSet):
+    queryset = TicketType.objects.all()
+
+    def list(self, request):
+        serializer = TicketTypeSerializer(self.queryset, many = True)
+        return Response(serializer.data)
