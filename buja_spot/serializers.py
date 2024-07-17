6ce83853a7abe5dc4c661c9729c7026c85b36c90
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2024-07-17 12:03:31 +0800

    lastest

--- buja_spot/serializers.py
+++ buja_spot/serializers.py
@@ -1,5 +1,43 @@
 from rest_framework import serializers
-from .models import Item,TicketType,EventType,EventCategory,Event,EventTicketType
+from .models import *
+from django.db.models import fields
+
+class TokenPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super(TokenPairSerializer, self).validate(attrs)
+        data['is_admin'] = self.user.is_superuser
+        data['groups'] = [x.name for x in self.user.groups.all()]
+        data['username'] = self.user.username
+        data['first_name'] = self.user.first_name
+        data['last_name'] = self.user.last_name
+        data['id'] = self.user.id
+
+        return data
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        read_only_fields = 'is_active', 'is_staff',
+        exclude = 'last_login', 'is_staff', 'date_joined', 'user_permission'
+
+        extra_kwargs = {
+            'username': {
+                'validators': [UnicodeUsernameValidator()]
+            }
+        }
+
+class RegisterSerializer(serializers.Serializer):
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+    email = serializers.CharField(required=True)
+
+class GroupSerializer(serializers.ModelField):
+    class Meta:
+        model =Group
+        fields = '__all__'
+
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
